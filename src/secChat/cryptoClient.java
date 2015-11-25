440bc5b2a8e6fa7eea819f299eeeafda291e3039
Charles Rathe <charles-rathe@uiowa.edu>
2015-11-25 17:37:58 -0600

    GUI implemented: super basic
    I also added some comments about future stuff we could do.

--- src/secChat/cryptoClient.java
+++ src/secChat/cryptoClient.java
@@ -1,22 +1,22 @@
 package secChat;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.event.*;
+
+
 
 public class cryptoClient {
 
 	// GUI Objects
 	private JTextField userInput = new JTextField(100);
-	private JTextArea messageDisplay = new JTextArea(8, 40);
+	private JTextArea messageDisplay = new JTextArea();
 	private JFrame frame = new JFrame("Crypto Messaging Client");
 	
 	// Chat objects
@@ -26,17 +26,30 @@ public class cryptoClient {
 	private String server;
 	
 	// Server Port for application
-	private static final int serverPort = 9960;
+	private static final int appPort = 9960;
 	
 	public cryptoClient()
 	{
 		// Sets both display area and text field to be uneditable until connection secured
 		userInput.setEditable(false);
 		messageDisplay.setEditable(false);
+		// NOTE: Third Display area may be added here for selecting user? Maybe add this later
+		/*
+		 * Things we would need:
+		 * 
+		 * Idea 1) Could have users and an add user button, this would require multiple panels, one of which would have to be layered.
+		 * 		   This would also require an add user button.
+		 * 		   JObjects which could be selected, each with its own listener in the Event listener section
+		 *         Preferably would change color (this might be something we could do if we have time which we probs won't
+		 *         
+		 * Idea 2) User inputs an IP address which he would like to connect to. Maybe a nickname too.
+		 * 		   Could store in a hash and allow the user to pick one in the future if necessary
+		 * 		   Simpler, don't even really need to store, but could be used to store the encryption key for future recognition
+		 */
 		
 		// Frame Layout
-		frame.getContentPane().add(userInput, "SOUTH");
-		frame.getContentPane().add(new JScrollPane(messageDisplay), "CENTER");
+		frame.getContentPane().add(userInput, BorderLayout.SOUTH);
+		frame.getContentPane().add(new JScrollPane(messageDisplay), BorderLayout.CENTER);
 		frame.pack();
 		
 		// Event Listener
@@ -49,8 +62,8 @@ public class cryptoClient {
 				
 				// Add Encryption Here??
 				
-				out.println(userInput.getText());
-				userInput.setText("");	
+				// out.println(userInput.getText());
+				// userInput.setText("");	
 			}
 		});
 	}
@@ -81,8 +94,9 @@ public class cryptoClient {
 		cryptoClient client = new cryptoClient();
 		client.frame.setVisible(true);
 		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.frame.setSize(1000, 400);
 		
-		client.run();	
+		// client.run();	
 	}
 
 }
