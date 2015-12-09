36ca29a9336290a9a7a4d5e51d26ab0d2624ab1e
Charles Rathe <charles-rathe@uiowa.edu>
2015-12-08 22:04:40 -0600

    Basic Messenger

--- src/secChat/CryptoClient.java
+++ src/secChat/CryptoClient.java
@@ -7,17 +7,24 @@ import java.net.*;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 
-public class CryptoClient extends SecWindow{
+public class CryptoClient extends JFrame{
+	
+	// GUI Objects
+	private JTextField textField;
+	private JTextArea displayArea;
+	private JTextField ipField;
 	
 	// Chat objects
-	private BufferedReader in;
-	private PrintWriter out;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
 	private Socket connection;
 	private String server = "127.0.0.1";
-	private String serverAddress;			// Stores IP Address
+	private String message = "";
 	
 	// Server Port for application
 	private static final int appPort = 9960;
@@ -26,49 +33,147 @@ public class CryptoClient extends SecWindow{
 	public CryptoClient()
 	{
 		super("Encrypted Chat");
-		mute();
-		muteChat();
 		
+		// GUI Setup
+		textField = new JTextField();
+		displayArea = new JTextArea();
+		ipField = new JTextField();
+		
+		// Set initial fields
+		textField.setEditable(false);
+		displayArea.setEditable(false);
+		ipField.setText("No connection");
+		ipField.setEditable(false);
+		
+		// Add ActionListener
+		textField.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent event)
+				{
+					sendData(event.getActionCommand());
+					textField.setText("");
+				}
+			}
+		);
+		
+		// Set Layout
+		add( ipField, BorderLayout.NORTH);
+		add( new JScrollPane(displayArea), BorderLayout.CENTER);
+		add( textField, BorderLayout.SOUTH);
+		
+		// Set window
+		setSize(300, 150);
+		setVisible(true);
 	}
+	
+	
 	public void run()
 	{
+		// Catch IO Exception on setup
 		try{
 			connectToServer();
 			getStreams();
 			processConnection();
 			
-			
 		} catch(EOFException e){}
 		catch(IOException io) {}
 		finally{closeConnection();}
 		
-		
-		
 	}
 	
+	// Connects to server (Hard-coded right now to be on this computer)
 	public void connectToServer() throws IOException
 	{
-		appendDisplay("Attempting Connection");
+		// Show that a connection is being attempted
+		displayMessage("Attempting Connection");
+	
+		// Attempt to connect to server
+		connection = new Socket(InetAddress.getByName(server), appPort);
+		
+		// Show if connected
+		displayMessage("Connected to " + connection.getInetAddress().getHostName());
+		ipField.setText("Connected to: " + server);
 	}
 	
 	public void getStreams()throws IOException
 	{
+		// Set up output stream, flush
+		output = new ObjectOutputStream(connection.getOutputStream());
+		output.flush();
 		
+		// Set up input stream
+		input = new ObjectInputStream(connection.getInputStream());
+		
+		// Display if successful
+		displayMessage("Streams successfully set up");
 	}
 	
 	public void processConnection() throws IOException
 	{
+		// allow user to type in field
+		textField.setEditable(true);
 		
+		do{
+			try{
+				message = (String) input.readObject();
+				displayMessage(message);
+			} catch( ClassNotFoundException cnfException){
+				displayMessage("Object of unknown type");
+			}
+			
+			
+			
+		} while (!message.equals("SERVER: TERMINATE"));
 	}
 	
 	public void closeConnection()
 	{
+		displayMessage("Closing connection...");
+		textField.setEditable(false);
+		ipField.setText("No connection");
 		
+		try{
+			output.close();
+			input.close();
+			connection.close();
+		} catch( IOException io)
+		{
+			io.printStackTrace();
+		}
+	}
+	
+	public void sendData( String message)
+	{
+		try{
+			output.writeObject("CLIENT: " + message);
+			output.flush();
+			displayMessage("CLIENT: " + message);
+			
+		} catch (IOException io)
+		{
+			displayArea.append("\nCould not write to Server");
+		}
+		
+	}
+	
+	public void displayMessage(final String m)
+	{
+		
+		SwingUtilities.invokeLater(
+				new Runnable()
+				{
+					public void run()
+					{
+						displayArea.append("\n" + m);
+					}
+				}
+				);
 	}
 	
 	public static void main(String[] args)
 	{
 		CryptoClient client = new CryptoClient();
+		client.run();
 		
 	}
 }
--- src/secChat/CryptoServer.java
+++ src/secChat/CryptoServer.java
@@ -3,21 +3,31 @@
  */
 package secChat;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.*;
+
 import javax.swing.*;
 
 /**
  * @author Charles
  *
  */
-public class CryptoServer extends SecWindow {
+public class CryptoServer extends JFrame {
+	
+	// GUI Objects
+	private JTextField textField;
+	private JTextArea displayArea;
+	private JTextField ipField;
 	
 	// Chat Objects
 	private ObjectOutputStream output;
 	private ObjectInputStream input;
 	private ServerSocket server;
 	private Socket connection;
+	private String message;
 	
 	// Port Number
 	private static final int appPort = 9960;
@@ -25,45 +35,80 @@ public class CryptoServer extends SecWindow {
 	public CryptoServer(){
 		
 		// Initialize window
-		super("SecChat Client");
+		super("SecChat Server");
+		
+		//GUI Setup
+		textField = new JTextField();
+		displayArea = new JTextArea();
+		ipField = new JTextField();
+		
+		// Add ActionListener to chat field
+		textField.addActionListener(
+				new ActionListener(){
+					public void actionPerformed(ActionEvent event){
+						sendData(event.getActionCommand());
+						textField.setText("");
+					}
+				}
+				);
+		
+		// Set Layout
+		add(ipField, BorderLayout.NORTH);
+		add(new JScrollPane(displayArea), BorderLayout.CENTER);
+		add(textField, BorderLayout.SOUTH);
+		
+		//Set window size
+		setSize(300, 150);
+		setVisible(true);
 		
 		// Set IP Fields
-		this.setIP("No user connected");
-		this.muteIP();
+		ipField.setText("Not Connected");
+		ipField.setEditable(false);
 	}
 	
 	public static void main(String[] args) {
 		
+		CryptoServer server = new CryptoServer();
+		server.run();
 
 	}
 	
 	// Run Server
-	void run() throws IOException
+	void run()
 	{
-		// Create server socket for connection
-		server = new ServerSocket(appPort);
 		
-		// Loop to keep waiting for connection
-		while(true)
+		try{
+		
+			// Create server socket for connection
+			server = new ServerSocket(appPort);
+			
+			// Loop to keep waiting for connection
+			while(true)
+			{
+				// Messaging protocol
+				try{
+					waitForConnections();
+					getStreams();
+					processConnection();
+					// verifyConnection();
+					
+				} catch (IOException e){displayMessage("\nConnection terminated\n");}
+				finally{
+					closeConnection();
+				}
+			}	
+		} catch (IOException io)
 		{
-			// Messaging protocol
-			try{
-				waitForConnections();
-				getStreams();
-				// verifyConnection();
-				
-			} catch (IOException e){displayMessage("\nConnection terminated\n");}
-			finally{
-				closeConnection();
-			}
-		}	
+			io.printStackTrace();
+		}
 	}
 	
 	// Waits for a connection from client
 	private void waitForConnections() throws IOException{
 		displayMessage("Waiting for connection...\n");
 		connection = server.accept();
-		displayMessage("Connection recieved from: " + connection.getInetAddress().getHostName());	
+		displayMessage("Connection recieved from: " + connection.getInetAddress().getHostName());
+		ipField.setText("Connected to: " + connection.getInetAddress().getHostName());
 	}
 	
 	// Gets the connection streams
@@ -81,10 +126,10 @@ public class CryptoServer extends SecWindow {
 	private void processConnection() throws IOException {
 		
 		// Send connection successful message
-		String message = "Connection Successful, Sending key";
-		sendMessage(message);
+		message = "Connection Successful, Sending key";
+		sendData(message);
 		
-		this.unmute();
+		
 		
 		do{
 			try{
@@ -94,7 +139,7 @@ public class CryptoServer extends SecWindow {
 			{
 				displayMessage("Unknown type recieved");
 			} 
-		} while (!message.equals( "CLIENT>>> TERMINATE"));
+		} while (!message.equals("CLIENT>>> TERMINATE"));
 		
 	}
 	
@@ -106,7 +151,7 @@ public class CryptoServer extends SecWindow {
 		SwingUtilities.invokeLater(
 			new Runnable(){
 				public void run()
-				{mute();}
+				{textField.setEditable(false);}
 			}
 		);
 	}
@@ -121,29 +166,21 @@ public class CryptoServer extends SecWindow {
 		SwingUtilities.invokeLater(
 				new Runnable(){
 					public void run(){
-					appendDisplay(m);
+					displayArea.append("\n" + m);
 			}
 		});
 	}
 	
 	// Sends message to client
-	private void sendMessage(String message){
+	private void sendData(String message){
 		try{
 			
-			output.writeObject("Server: " + message);
+			output.writeObject("SERVER: " + message);
 			output.flush();
-			displayMessage("\nServer: " + message);
-		} catch (IOException e){appendDisplay("Error sending message! \n");}
+			displayMessage("Server: " + message);
+		} catch (IOException e){displayMessage("Error sending message! \n");}
 		
 	}
 	
-	/*private void sendKey(String key){
-		try{
-			output.writeObject(key);
-			output.flush();
-			displayMessage("Key Sent\n");
-			
-		}catch (IOException e){window.appendDisplay("Error sending message! \n");}
-	}
-	*/
+
 }
